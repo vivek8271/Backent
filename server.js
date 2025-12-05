e062a32f2225e14db1f9a42052af4f4483b80428
vivek8271 <vivekkumar37904@gmail.com>
2025-12-05 19:28:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 const os = require('os');
@@ -9,12 +9,41 @@ app.use(cors());
 app.use(express.json());
 
 
-mongoose.connect('mongodb://localhost:27017/firstdb')
-  .then(() => console.log('✅ MongoDB connected'))
-  .catch(err => console.error('❌ MongoDB error:', err));
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.cqpn3dg.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+// mongoose.connect('mongodb://localhost:27017/firstdb')
+//   .then(() => console.log('✅ MongoDB connected'))
+//   .catch(err => console.error('❌ MongoDB error:', err));
 
 // Schema
-const FormData = mongoose.model('FormData', new mongoose.Schema({
+const FormData = MongoClient.model('FormData', new MongoClient.Schema({
   name: String,
   mobile_no: String,
   email: String,
@@ -36,10 +65,14 @@ app.post('/submit-form', async (req, res) => {
 });
 
 
+
+
+
 // Server
-app.listen("https://aditienterprises.vercel.app", () => console.log('🚀 Server running on vercel'));
+// app.listen("https://aditienterprises.vercel.app", () => console.log('🚀 Server running on vercel'));
 //   }
 // });
 
 // app.listen(8080, () => console.log('Server listening on 8080'));
+
 
